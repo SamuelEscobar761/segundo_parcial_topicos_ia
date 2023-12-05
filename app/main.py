f6336618c6318548947aba2f02bab73ed364bf4c
¨lizarazu22¨ <lizarazuaramayoignacio@gmail.com>
2023-12-05 13:35:24 -0400

    commit

--- app/main.py
+++ app/main.py
@@ -2,56 +2,61 @@ import os
 import spacy
 import time
 import psutil
-from typing import Union
+import csv
+import datetime
 from fastapi import FastAPI
-from predictor import SentimentClassifier  # Importa la clase desde tu predictor.py
+from predictor import SentimentClassifier
 
 app = FastAPI()
 nlp = spacy.load("es_core_news_sm")
-
-
-# Get the current directory of the script
 current_directory = os.path.dirname(__file__)
-
-# Relative path to the directory containing your model and tokenizer files
 directory_path = os.path.join(current_directory, "code")
-
-# Crea una instancia de SentimentClassifier con las rutas correctas para tu modelo y tokenizer
 classifier = SentimentClassifier(model_path=directory_path, tokenizer_path=directory_path)
+
+# Función para registrar en CSV
+def log_to_csv(endpoint, text, response):
+    with open('reports.csv', mode='a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        now = datetime.datetime.now()
+        writer.writerow([now, endpoint, text, response])
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.post("/sentiment/")  # Nueva ruta para la predicción de sentimientos
+@app.get("/status")
+def get_status():
+    status_info = {
+        "service_status": "running",
+        "system_info": {
+            "cpu_usage": psutil.cpu_percent(),
+            "memory_usage": psutil.virtual_memory().percent
+        },
+        "model_info": {
+            "sentiment_model": "AutoModelForSequenceClassification",
+            "nlp_model": "es_core_news_sm"
+        }
+    }
+    return status_info
+
+@app.post("/sentiment/")  
 def predict_sentiment(text: str):
-    predicted_sentiment = classifier.predict(text)  # Utiliza el método predict de la clase SentimentClassifier
-
-    return {"Predicted Sentiment": predicted_sentiment}
-
+    predicted_sentiment = classifier.predict(text) 
+    response = {"Predicted Sentiment": predicted_sentiment}
+    log_to_csv("/sentiment", text, response)
+    return response
 
 @app.post("/analysis")
 async def analyze_text(text: str):
-    start_time = time.time()  # Tiempo de inicio de la ejecución
-
-    # Realizar análisis de texto usando SpaCy
+    start_time = time.time()
     doc = nlp(text)
-
-    end_time = time.time()  # Tiempo de finalización de la ejecución
-    execution_time = end_time - start_time  # Tiempo total de ejecución
-
-    # Obtener análisis de sentimiento (solo un ejemplo, puedes usar otra librería o método)
-    sentiment_value = classifier.predict(text)  # Esto sería el valor de sentimiento calculado
-    sentiment_category = classifier.get_category(sentiment_value)  # Aquí se asignaría la categoría de sentimiento
-
-    # Calcular tamaño del texto procesado (carácteres)
+    end_time = time.time()
+    execution_time = end_time - start_time
+    sentiment_value = classifier.predict(text)
+    sentiment_category = classifier.get_category(sentiment_value)
     text_size = len(text)
-
-    # Calcular la memoria utilizada por el proceso
     process = psutil.Process()
-    memory_used = process.memory_info().rss  # Memoria utilizada en bytes
-
-    # Recopilar información de la predicción
+    memory_used = process.memory_info().rss
     prediction_info = {
         "POS_tags": [token.pos_ for token in doc],
         "NER": [(ent.text, ent.label_) for ent in doc.ents],
@@ -59,23 +64,22 @@ async def analyze_text(text: str):
         "Sentiment_Value": sentiment_value,
         "Sentiment_Category": sentiment_category
     }
-
-    # Información de la ejecución de la predicción
     execution_info = {
-        "Execution_Time": execution_time,  # Tiempo total de ejecución en segundos
-        "Text_Size": text_size,  # Tamaño del texto procesado, número de carácteres
-        "Memory_Used": memory_used  # Memoria utilizada por el proceso en bytes
+        "Execution_Time": execution_time,
+        "Text_Size": text_size,
+        "Memory_Used": memory_used
     }
-
-    # Combinar información de la predicción y ejecución
     response = {
         "Prediction_Info": prediction_info,
         "Execution_Info": execution_info
     }
-
+    log_to_csv("/analysis", text, response)
     return response
 
-
+@app.get("/reports")
+def get_reports():
+    # Aquí puedes implementar la lógica para descargar el archivo CSV
+    pass
 
 if __name__ == "__main__":
     import uvicorn
